5a6b73b9db02a98f4fbe9b854a30596b6d6b9620
jrbertt <joaorebertt0219@gmail.com>
2025-05-05 23:11:49 -0300

    feat(schema): add name field to IpAddress model

--- prisma/migrations/20250506014727_add_name_ip/migration.sql
+++ prisma/migrations/20250506014727_add_name_ip/migration.sql
@@ -1,12 +1,1 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `ipAddress` on the `query_logs` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "IpAddress" ADD COLUMN     "name" TEXT;
-
--- AlterTable
-ALTER TABLE "query_logs" DROP COLUMN "ipAddress",
-ADD COLUMN     "ip_address" TEXT;
+ALTER TABLE "IpAddress" ADD COLUMN "name" TEXT;
